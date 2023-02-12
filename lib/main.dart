54c07baba472551aef36bdacbfb21aa11e99154c
AliAltyyev <alidroid696@gmial.com>
2023-02-12 16:01:19 +0300

    Showing Spinner while user waits

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:weather_app/screens/home.dart';
+import 'package:weather_app/screens/splash.dart';
 
 void main() {
   runApp(const MyApp());
