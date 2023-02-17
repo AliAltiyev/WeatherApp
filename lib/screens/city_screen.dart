cd53ca4016f157d113979ddac3534865a31e66b0
AliAltyyev <alidroid696@gmial.com>
2023-02-17 10:01:53 +0300

    add textField for searching city

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -12,13 +12,33 @@ class _CityScreenState extends State<CityScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          title: Text("Search"),
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+        ),
         body: Container(
-      decoration: BoxDecoration(gradient: kAppBackgroundGradientColor),
-
-
-    )
-
-
-    );
+          decoration: BoxDecoration(gradient: kAppBackgroundGradientColor),
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 40),
+                child: TextField(
+                  onChanged: (String value) {
+  debugPrint(value);
+                  },
+                  decoration: kCitySearchTextFieldDecoration,
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              SizedBox(
+                height: 40,
+                width: 200,
+                child: ElevatedButton(
+                    onPressed: () {}, child: const Text('Search')),
+              )
+            ],
+          ),
+        ));
   }
 }
--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -6,3 +6,9 @@ const kAppBackgroundGradientColor = LinearGradient(
     colors: [Color(0xff1E1E1E), Color(0xff1e1e5e)],
     begin: Alignment.topCenter,
     end: Alignment.bottomCenter);
+
+const kCitySearchTextFieldDecoration = InputDecoration(
+    filled: true,
+    fillColor: Colors.white,
+    hintText: 'Enter city',
+    hintStyle: TextStyle(color: Colors.black));
